aaea4ca4b45c98d804dac401e8cea3635fc1396f
oakixz <oakixz@gmail.com>
2025-10-03 12:29:17 +0700

    Update path.js

--- frontend/config/path.js
+++ frontend/config/path.js
@@ -1,4 +1,4 @@
-const API = `http://localhost:5000/api`;
+const API = `https://apply-xmg7.onrender.com/api`;
 const api_user = `${API}/user`;
 const api_auth = `${API}/auth`;
 const api_teacher = `${API}/teacher`;
