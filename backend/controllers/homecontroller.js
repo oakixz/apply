c7b6952db42b4b6ddc2d5e4c309a419ad2227792
oakixz <oakixz@gmail.com>
2025-10-03 14:50:31 +0700

    Update homecontroller.js

--- backend/controllers/homecontroller.js
+++ backend/controllers/homecontroller.js
@@ -1,8 +1,10 @@
-export const home = (req, res) => {
+import pool from "../pool.js";
+
+export const home = async (req, res) => {
   try {
-    const [rows] = pool.query("SELECT NOW() as now");
+    const [rows] = await pool.query("SELECT NOW() as now");
     res.json({ success: true, time: rows[0].now });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
   }
-});
+};
